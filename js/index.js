830422de580fb03c391ab48c77b5b0a980eeb4fc
mgbrando <mgbrando29@gmail.com>
2017-03-21 00:14:53 -0400

    Added reducers

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import * as actions from './actions/index';
+import * as reducers from './reducers/index';
+
+console.log(reducers.repositoryReducer(undefined, actions.addRepository('myRepository')));
+console.log(actions.addRepository('myRepository'));
+/*const lists = ["Hot List", "Warm List", "Cold List"];
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<Board title="The Board" lists={lists} />, document.getElementById('app'))
+);*/
